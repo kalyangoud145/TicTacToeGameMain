82cd3ba55ba4856718fc070cca7d0f8dafad3e55
kalyangoud145 <kalyangoud145@gmail.com>
2020-09-26 11:00:17 +0530

    created a empty board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to TicTacToe game"
+NULL=" "
+declare -a board
+function createBoard()
+{
+   for (( index=1; index<=9; index++ ))
+   do
+      board[$index]=$NULL
+   done
+}
+function TicTacToeGame(){
+   createBoard
+}
+TicTacToeGame
+
 
